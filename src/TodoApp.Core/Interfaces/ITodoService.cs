56d2c37bbc14d98e6751ee5286a950d7aa9511b8
Joshua Varga <joshua.varga@prolucid.ca>
2026-04-07 14:01:46 -0400

    add multiple lists

--- src/TodoApp.Core/Interfaces/ITodoService.cs
+++ src/TodoApp.Core/Interfaces/ITodoService.cs
@@ -5,6 +5,11 @@ namespace TodoApp.Core.Interfaces;
 public interface ITodoService
 {
     IObservable<IReadOnlyList<TodoItem>> Todos { get; }
+    IObservable<IReadOnlyList<TodoList>> Lists { get; }
+
+    void SelectList(Guid listId);
+    TodoList CreateList(string name);
+    void DeleteList(Guid listId);
 
     void Add(string title);
     void Toggle(Guid id);
--- src/TodoApp.UI/ViewModels/MainViewModel.cs
+++ src/TodoApp.UI/ViewModels/MainViewModel.cs
@@ -1,5 +1,6 @@
 using System.Reactive;
 using System.Reactive.Linq;
+using System.Collections.ObjectModel;
 using ReactiveUI;
 using TodoApp.Core.Interfaces;
 using TodoApp.Core.Models;
@@ -10,40 +11,74 @@ public class MainViewModel : ReactiveObject
 {
     private readonly ITodoService _todoService;
 
-    private string _newTodoText = "";
-    public string NewTodoText
+    public ObservableCollection<TodoListTabViewModel> Tabs { get; } = [];
+
+    private TodoListTabViewModel? _selectedTab;
+    public TodoListTabViewModel? SelectedTab
     {
-        get => _newTodoText;
-        set => this.RaiseAndSetIfChanged(ref _newTodoText, value);
+        get => _selectedTab;
+        set
+        {
+            this.RaiseAndSetIfChanged(ref _selectedTab, value);
+            if (value is not null && value.IsAddingTasks)
+            {
+                _todoService.SelectList(value.ListId);
+            }
+        }
     }
 
-    private IReadOnlyList<TodoItem> _todos = [];
-    public IReadOnlyList<TodoItem> Todos
-    {
-        get => _todos;
-        private set => this.RaiseAndSetIfChanged(ref _todos, value);
-    }
+    public ReactiveCommand<Unit, Unit> AddTabCommand { get; }
+    public ReactiveCommand<TodoListTabViewModel, Unit> SelectTabCommand { get; }
 
-    public ReactiveCommand<Unit, Unit> AddTodoCommand { get; }
-    public ReactiveCommand<Guid, Unit> ToggleTodoCommand { get; }
-    public ReactiveCommand<Guid, Unit> RemoveTodoCommand { get; }
+    private string _newListName = "";
+    public string NewListName
+    {
+        get => _newListName;
+        set => this.RaiseAndSetIfChanged(ref _newListName, value);
+    }
 
     public MainViewModel(ITodoService todoService)
     {
         _todoService = todoService;
 
-        var canAdd = this.WhenAnyValue(x => x.NewTodoText)
-            .Select(text => !string.IsNullOrWhiteSpace(text));
+        // Load existing lists into tabs
+        _todoService.Lists
+            .Take(1)
+            .Subscribe(lists =>
+            {
+                foreach (var list in lists)
+                {
+                    Tabs.Add(new TodoListTabViewModel(_todoService, list.Id, list.Name));
+                }
+                if (Tabs.Count > 0) SelectedTab = Tabs[0];
+            });
 
-        AddTodoCommand = ReactiveCommand.Create(() =>
+        AddTabCommand = ReactiveCommand.Create(() =>
         {
-            _todoService.Add(NewTodoText);
-            NewTodoText = "";
-        }, canAdd);
+            // Add a placeholder tab in "naming" mode
+            var tempId = Guid.Empty;
+            var tab = new TodoListTabViewModel(_todoService, tempId, "New List", isAddingTasks: false);
+            Tabs.Add(tab);
+            SelectedTab = tab;
+        });
 
-        ToggleTodoCommand = ReactiveCommand.Create<Guid>(id => _todoService.Toggle(id));
-        RemoveTodoCommand = ReactiveCommand.Create<Guid>(id => _todoService.Remove(id));
+        SelectTabCommand = ReactiveCommand.Create<TodoListTabViewModel>(tab =>
+        {
+            SelectedTab = tab;
+        });
+    }
 
-        _todoService.Todos.Subscribe(todos => Todos = todos);
+    public void ConfirmNewList(TodoListTabViewModel tab, string name)
+    {
+        var created = _todoService.CreateList(name);
+        var index = Tabs.IndexOf(tab);
+        if (index >= 0)
+        {
+            var newTab = new TodoListTabViewModel(_todoService, created.Id, created.Name);
+            Tabs[index] = newTab;
+            SelectedTab = newTab;
+            _todoService.SelectList(created.Id);
+        }
     }
 }
+
